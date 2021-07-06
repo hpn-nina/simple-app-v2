041814438640643b36343597a439620a510384cb
lialic <blue0lilac@gmail.com>
2021-07-06 14:05:34 +0700

    Done logs page

--- packages/ec-web/components/Transaction/TransactionCard.js
+++ packages/ec-web/components/Transaction/TransactionCard.js
@@ -4,6 +4,7 @@ import {SeperatePrice} from '../../utils/format'
 import Link from 'next/link'
 import AuthContext from '../../context/AuthContext';
 import fecth from 'isomorphic-fetch'
+import  Router  from 'next/router';
 
 export default function TransactionCard(props) {
     const {transaction, count} = props;
@@ -19,7 +20,25 @@ export default function TransactionCard(props) {
             },
             body: JSON.stringify({transactionStatus: "Cancel"})
         })
-        alert('Hủy đơn hàng thành công');
+        if(res1.status === 200) {
+            alert('Hủy đơn hàng thành công');
+            Router.push('/users/myTransactions');
+        }
+    }
+
+    async function handleAccept(e) {
+        const res1 = await fecth(`${process.env.API_URL}/transactions/${transaction._id}`,{
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization" : `Bearer ${jwt}`
+            },
+            body: JSON.stringify({transactionStatus: "WorkingOn"})
+        })
+        if (res1.status === 200) {
+            alert('Nhận đơn hàng thành công');
+            Router.push('/users/myTransactions');
+        }
     }
 
     return (
@@ -41,13 +60,13 @@ export default function TransactionCard(props) {
                 transaction.transactionStatus === 'New' ? (
                     <div className='center btn-zone'>
                         <div className='btn-container'>
+                            <Button onClick={(e) => handleAccept(e)} className='Button'>Nhận đơn hàng này</Button>
+                        </div>
+                        <div className='btn-container'>
                             <Button variant='danger' onClick={(e) => handleDelete(e)}>Hủy đơn hàng</Button>
                         </div>
                         <div className='btn-container'>
                             <Link href='/users/message'><a><Button variant='secondary' className='Button'>Nhắn tin với Freelancer</Button></a></Link>
-                        </div>
-                        <div className='btn-container'>
-                            <Link href={`/jobs/${transaction.job.id}`}><a><Button className='Button'>Xem công việc này</Button></a></Link>
                         </div>
                     </div>
                 ) : (transaction.transactionStatus === 'WorkingOn' ? (
--- packages/ec-web/pages/users/jobs/transactions/logs.js
+++ packages/ec-web/pages/users/jobs/transactions/logs.js
@@ -4,14 +4,108 @@ import Router from 'next/router'
 import { useContext, useState } from 'react'
 import AuthContext from '../../../../context/AuthContext'
 import { getSession } from 'next-auth/client'
-import { Row, Col, Button } from 'react-bootstrap'
-import fecth from 'isomorphic-fetch'
+import { Row, Col, Button, Carousel } from 'react-bootstrap'
+import {SeperatePrice} from '../../../../utils/format'
+
 
 export default function logs(props) {
-    console.log(props.session)
+    var count = 0;
+    var countImage = 0;
     return (
         <div className='container'>
+            <div className='title'>Những thông tin cập nhật về đơn hàng</div>
+            <div className='sm-title'>Thông tin đơn hàng</div>
+            <Row>
+                <Col>
+                    <div>Mã đơn hàng: <div className='bold inline'>{props.transaction.id}</div></div>
+                </Col>
+                <Col>
+                    <div>Tình trạng đơn hàng: <div className='bold inline'>{props.transaction.transactionStatus}</div></div>
+                </Col>
+            </Row>
             
+            <Row>
+                <Col>
+                    <div>Số tiền: <div className='bold inline'>{SeperatePrice(props.transaction.amount)}</div></div>
+                </Col>
+                <Col>
+                    <div>Lựa chọn công việc đã chọn: <div className='bold inline'>{props.transaction.pickedOption.optionName}</div></div>
+                </Col>
+            </Row>
+            <div>Ghi chú: {props.transaction.note ? props.transaction.note : 'Không có'}</div>
+            <div className='sm-title'>Thông tin công việc</div>
+            <Row>
+                <Col>
+                    <div>Mã công việc: <div className='bold inline'>{props.transaction.job._id}</div></div>
+                </Col>
+                <Col>
+                    <div>Tên công việc: <div className='bold inline'>{props.transaction.job.name}</div></div>
+                </Col>
+            </Row>
+            <div>Mô tả công việc: {props.transaction.job.desc}</div>
+            <div className='sm-title'>Thông tin cập nhật về đơn hàng</div>
+            {
+                props.transaction.updatedLogs.map(log => (
+                    <div>
+                        <Row style={{marginBottom: '2%'}}>
+                            <Col><div className='bold inline'>Cập nhật {++count}</div></Col>
+                            <Col>
+                                <div>Ghi chú: {log.note ? log.note : 'Không có'}</div>
+                            </Col>
+                            <Col>
+                                <div>Cập nhật giai đoạn: {log.proof}</div>
+                            </Col>
+                        </Row>
+                        <Row style={{marginBottom: '5%'}}>
+                            <div className='bottom'>Hình ảnh cập nhật:</div> 
+                            <Carousel>
+                             {
+                                log.image.length === 0 ? <div className='inline'>Không có</div> : (
+                                    log.image.map(img => (
+                                        <Carousel.Item interval={5000}>
+                                            <img className='d-block w-100'
+                                                src={process.env.API_URL + img.url}
+                                                alt={`Ảnh ${++countImage}`}
+                                            />
+                                        </Carousel.Item>
+                                    ))
+                                )
+                            }
+                            </Carousel>
+                        </Row>
+                    </div>
+                ))
+            }
+            <div>
+                {
+                    props.transaction.freelancer.id === props.session.id && props.transaction.transactionStatus != 'Finish' ? (
+                        <Row>
+                            <Col>
+                                <div className='center'><Link href={`/users/jobs/transactions/about?id=${props.transaction.id}`}><a><Button>Thêm thông tin cập nhật</Button></a></Link></div>
+                            </Col>
+                            <Col>
+                            <div className='center'><Link href={`/users/jobs/transactions/done?id=${props.transaction.id}`}><a><Button variant='dark'>Cập nhật thông tin hoàn thành đơn hàng</Button></a></Link></div>
+                            </Col>
+                        </Row>
+                    ) : ''
+                }
+            </div>
+            <style jsx>
+                {`
+                .sm-title{
+                    margin: 3%;
+                    font-size: 1.4rem;
+                    font-weight: 700;
+                }
+                .center {
+                    margin: 5% 0px;
+                }
+                .bottom{
+                    margin-bottom: 5%;
+                }
+                `}
+            </style>
+        
         </div>
     )
 }
@@ -19,11 +113,13 @@ export default function logs(props) {
 export const getServerSideProps = async({req, res, query}) => {
     const session = await getSession({req});
     const transactionId = query.transaction;
+    const {API_URL} = process.env;
 
-    const transaction = await fecth(`${process.env.API_URL}/transactions/${transactionId}`,{
-        method: "GET",
+    const transaction = await fetch(`${API_URL}/transactions/${transactionId}`, {
         headers: {
-            "Authorzation" : `Bearer ${session.jwt}`
+            "Accept": "application/json",
+            "Content-Type": "application/json", 
+            "Authorization": `Bearer ${session.jwt}`
         }
     })
     const data = await transaction.json();
